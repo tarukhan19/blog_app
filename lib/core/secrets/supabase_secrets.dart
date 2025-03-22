074111fb3ed065fe0782b6a38d7c11603b242ba9
taru <tarukhan19@gmail.com>
2025-03-22 21:00:48 +0530

    working code setup supabase

---
+++ lib/core/secrets/supabase_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets{
+  static const SUPA_BASE_URL = "https://ybrpbicqkfjcuujztdxr.supabase.co";
+  static const SUPA_BASE_ANNON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlicnBiaWNxa2ZqY3V1anp0ZHhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2NTUxMjIsImV4cCI6MjA1ODIzMTEyMn0.pL5O2uvpLoHQo7SCIr70IjUGdVw87XOXZXX0ga8ewaA";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/screens/screen_signin.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+import 'core/secrets/supabase_secrets.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+ final supaBase =  await Supabase.initialize(url: AppSecrets.SUPA_BASE_URL, anonKey: AppSecrets.SUPA_BASE_ANNON_KEY);
+
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
